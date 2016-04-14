072317614563302633ed93197b604435be899079
Valerie Burzynski <valerie@bendyworks.com>
2016-04-14 16:56:55 -0500

    redirect to question path when create successful

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,7 +10,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
     if @question.save
-
+      redirect_to @question
     else
       render 'new'
     end
