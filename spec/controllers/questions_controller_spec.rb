f0cb76d4ff33f28245c71348e23595f9fd85a076
Valerie Burzynski <valerie@bendyworks.com>
2016-04-14 16:57:39 -0500

    Add tests to question controller spec to validate create action

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -7,6 +7,34 @@ RSpec.describe QuestionsController, type: :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
+
+    it "shows invalid question submission information" do
+      get :new
+      assert_template 'questions/new'
+    end
   end
 
+  describe "POST 'create'" do
+    describe "with invalid arguments" do
+      before(:each) do
+        request.env["HTTP_REFERER"] = new_question_path
+        @invalid_args = {
+          prompt: "",
+          correct_feedback: "",
+          incorrect_feedback: ""
+        }
+      end
+
+      it "should return a non-empty list of errors" do
+        post 'create', question: @invalid_args
+        expect(assigns(:question).errors.empty?).to_not be true
+      end
+
+      it "should not modify the number of questions" do
+        question_count = Question.count
+        post 'create', question: @invalid_args
+        expect(question_count).to eq Question.count
+      end
+    end
+  end
 end
