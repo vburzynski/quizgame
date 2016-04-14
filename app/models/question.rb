cd8bf4b65bd1325a66b940d9567e90562caef76c
Valerie Burzynski <valerie@bendyworks.com>
2016-04-14 16:08:52 -0500

    Disallow blank strings on Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
-  validates :prompt, presence: true
-  validates :incorrect_feedback, presence: true
-  validates :correct_feedback, presence: true
+  validates :prompt, presence: true, allow_blank: false
+  validates :incorrect_feedback, presence: true, allow_blank: false
+  validates :correct_feedback, presence: true, allow_blank: false
 end
