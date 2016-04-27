45c19686aedc9b2de33acf02b01eada51a7db642
Valerie Burzynski <valerie@bendyworks.com>
2016-04-27 11:49:27 -0500

    Add tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(create(:user)).to be_valid
+  end
+  it "is invalid without an email" do
+    expect(build(:user, email: nil)).to_not be_valid
+  end
+  it "is invalid without a password" do
+    expect(build(:user, password: nil)).to_not be_valid
+  end
+  it "is invalid without a verified password" do
+    expect(build(:user, password: "password1", password_confirmation: "password2")).to_not be_valid
+  end
+  it "is invalid with a duplicate email address" do
+    user1 = create(:user)
+    expect(build(:user, email: user1.email)).to_not be_valid
+  end
 end
