5529f96bd4c195916dfb9ca44dc7af8c67b90f4c
Valerie Burzynski <valerie@bendyworks.com>
2016-04-27 16:30:06 -0500

    Test PageController

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,10 +3,24 @@ require 'rails_helper'
 RSpec.describe PagesController, type: :controller do
 
   describe "GET #secret" do
-    it "returns http success" do
-      get :secret
-      expect(response).to redirect_to(new_user_session_path)
+    describe "anonymous user" do
+      it "returns http status 302" do
+        get :secret
+        expect(response).to have_http_status(302)
+      end
+      it "redirects to user login" do
+        get :secret
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+    describe "authenticated user" do
+      before :each do
+        login_with create( :user )
+      end
+      it "returns http success" do
+        get :secret
+        expect(response).to have_http_status(:success)
+      end
     end
   end
-
 end
--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -1,9 +1,19 @@
 module ControllerHelpers
 
+  def login_user
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      user = FactoryGirl.create(:user)
+      user.confirm
+      sign_in user
+    end
+  end
+
   def login_with(user = double('user'), scope = :user)
     current_user = "current_#{scope}".to_sym
     if user.nil?
-      allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, {:scope => scope})
+      allow(request.env['warden']).to receive(:authenticate!)
+        .and_throw(:warden, {:scope => scope})
       allow(controller).to receive(current_user).and_return(nil)
     else
       allow(request.env['warden']).to receive(:authenticate!).and_return(user)
