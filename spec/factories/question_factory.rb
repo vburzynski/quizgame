52c231ee346bb12ee8ee3819c37d38d0c46b4cc7
Valerie Burzynski <valerie@bendyworks.com>
2016-04-27 14:29:13 -0500

    Add factory to create question with options

--- spec/factories/question_factory.rb
+++ spec/factories/question_factory.rb
@@ -5,5 +5,13 @@ FactoryGirl.define do
     f.prompt {Faker::Lorem.sentence}
     f.correct_feedback {Faker::Lorem.words(3)}
     f.incorrect_feedback {Faker::Lorem.words(3)}
+
+    factory :question_with_options do |f|
+      after(:create) do |question|
+        3.times do |n|
+          create(:question_option, question: question)
+        end
+      end
+    end
   end
 end
