8aa768b9263ad204372092b4194af6623d1676d1
Valerie Burzynski <valerie@bendyworks.com>
2016-04-27 14:29:29 -0500

    Add tests for QuestionOption model

--- spec/models/question_option_spec.rb
+++ spec/models/question_option_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe QuestionOption, type: :model do
-  it "is valid with content and correct status"
-  it "is invalid without content"
-  it "is invalid without correct status"
+  it "it has a valid factory" do
+    expect(create(:question_option)).to be_valid
+  end
+  it "is invalid without content" do
+    expect(build(:question_option, content: "")).to_not be_valid
+  end
+  it "is invalid without correct status" do
+    expect(build(:question_option, correct: nil)).to_not be_valid
+  end
+  it "is invalid without association to question" do
+    expect(build(:question_option, question_id: nil)).to_not be_valid
+  end
 end
